68f77f8e14adfef27719ed109f5c3604e97f2d34
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-06-05 10:53:59 +0200

    Remove all Koa files to migrate to site static version

--- config/test.js
+++
@@ -1,5 +0,0 @@
-var config = require('./default.json');
-
-config.database.name = config.database.name + '_test';
-
-module.exports = config;
--- controllers/index.js
+++
@@ -1,5 +0,0 @@
-var koaMount = require('koa-mount');
-
-module.exports = function(app) {
-    app.use(koaMount('/', require('./landing-page')));
-};
